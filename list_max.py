6233355b50da0fb4cebe2efb006f90d1f8f2e3b3
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-11-06 23:48:48 -0800

    list_max.py

--- list_max.py
+++ list_max.py
@@ -2,15 +2,13 @@
 # GitHub username: huynhvan126
 # Date: 11/06/2024
 # Description: Write a recursive function that takes as its parameter a list of numbers and returns the max value in the list.
-def list_max(lst, index=0):
+def list_max(numbers):
     """
     Returns the maximum value in a list.
     """
-    if index == len(lst) - 1:
-        return lst[index]
+    if len(numbers) == 1:
+        return numbers[0]
 
     # Recursive case: get the max of the rest of the list
-    max_of_rest = list_max(lst, index + 1)
-
-    # Return the max of the current element and the max of the rest
-    return lst[index] if lst[index] > max_of_rest else max_of_rest
+    max_of_rest = list_max(numbers[1:])
+    return numbers[0] if numbers[0] > max_of_rest else max_of_rest
